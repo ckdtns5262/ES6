54a6ebb001119b0beac46e56ed8007ff02fd2f30
ckdtns5262 <ckdtns5262@nate.com>
2022-09-26 18:07:42 +0900

    [09.26] 자바스크립트 함수사용법

---
+++ 01/scripts/index.js
@@ -0,0 +1,17 @@
+// 기존 함수 선언
+//* 
+//function funPrint(){
+//   alert("반갑습니다");
+//   console.log("버튼1클릭");
+// }
+// function funPrint2(){
+//   alert("수고하셨습니다");
+//   console.log("버튼2클릭");
+// *}
+
+//ES6 : 화살표 함수
+const funPrint = (n) => {
+  // console.log( n+"번 클릭");
+    console.log(`${n}번클릭`)
+  
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# JavaScript
+# JavaScript 
+## JavaScript
++ script 태그사용
+  + 내부
+    + head 태그 영역
+    + body 태그 어디든 가능
+  + 외부
+    + src 속성
++ 화살표 함수
+```javascript
+  const function = (매개변수(파라미터))=> {
+    return값 (`${n}번클릭`)
+  }
+```
