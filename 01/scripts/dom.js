4178f7cd87dbfe36f4a77967f035117898a41c2d
ckdtns5262 <ckdtns5262@nate.com>
2022-09-29 18:06:03 +0900

    [09.29] 자바스크립트 DOM 연습

--- 01/scripts/dom.js
+++ 01/scripts/dom.js
@@ -1,56 +1,52 @@
-//function 키워드로 함수 생성
-//  function show(){
-//   console.log("show");
-// }
+//랜덤수
+let ranNum;
+// 사용자가 선택한 수
+let userNum;
 
-// 화살표 함수형식으로 생성 : 함수 내 명령이 하나이면 {} 생략가능
-//                          return문 하나이면 return도 생략가능
-// const show = ()=> console.log("show 중괄호 생략")
+// 화면 보기 함수
+const dispDom = (msgP, inNumP, bt1P, bt2P) => {
+  //DOM 제어
+  document.getElementById("msg").style.display = msgP;
+  document.getElementById("inNum").style.display = inNumP;
+  document.getElementById("bt1").style.display = bt1P;
+  document.getElementById("bt2").style.display = bt2P;
+};
 
-// 화살표 함수형식으로 생성
-const show = () => {
+// 주사위
+const diceShow = () => {
+  //랜덤수 생성
+  ranNum = Math.floor(Math.random() * 6) + 1;
+  console.log(`주사위 랜덤수 => ${ranNum}`);
 
-  const msgId = document.querySelector("#msg");
-  // msgId.innerHTML=("<h2>페이스북</h2>");
-  // console.log(msgId.innerHTML);
+  //DOM 제어
+  dispDom("none", "block", "none", "block");
+};
+// 확인
+const okShow = () => {
+  userNum = form1.num.value;
+  console.log(`사용자 선택 확인 수 => ${userNum}`);
+  //DOM 제어
+  dispDom("block", "none", "block", "none");
 
-  // msgId.innerText="<h2>페이스북</h2>"
-  // console.log(msgId.innerText);
-
-  // msgId.innerContent ="<h2>페이스북</h2>";
-  // console.log(msgId.innerContent);
-
-  //태그 만들기
- 
-  //1. 태그만들기
-  let tag = "";
-  /*
-  for(let i=1; i <= 6; i++) { 
-    //tag = tag + '<div class="divMsg">' + i + '</div>' ;
-    //템플릿문자열
-    tag = tag + `<div class="divMsg"><img src="./images/${i}.png" id="msdImgId${i}"></div>`
-  }
-  */
-
-  let i = Math.floor(Math.random() * 6) + 1;
-  tag = `<div class="divMsg"><img src="/images/${i}.png" id="msgImg"></div>`
-  console.log(tag);
-
-  //2.DOM에서 태그를 넣을 요소 선택
-  const msgDiv = document.querySelector('#msg');
-
-  //3.선택된 요소의 HTML 변경
-  msgDiv.innerHTML = tag;
+  // 주사위 그림
+  let tag = `<img src="/images/${ranNum}.png">`;
+  document.getElementById("msg").innerHTML = tag;
   
-  //4.동적으로 생성된 HTML요소도 스타일 변경 가능
-  let msgImg = document.querySelector('#msgImg') 
-  msgImg.style.maxWidth = "100px" ;
-  msgImg.style.maxheight = "100px" ;
+  
+  //ox 그림
+  let ox;
+  if (ranNum == userNum) {
+    tag = `{tag}<img src="/images/o.png">`;
+  } else {
+    tag = `{tag}<img src="/images/x.png">`;
   }
+};
 
-/* DOM 요소가 생성된 후에 실행*/
+//DOM이 생성이 되고 난 후에 요소를 CRUD 해야함.
 document.addEventListener("DOMContentLoaded", () => {
-  
-  
+  // 주사위 버튼만 보여야 함
+
+  document.getElementById("msg").style.display = "none";
+  document.getElementById("inNum").style.display = "none";
+  document.getElementById("bt2").style.display = "none";
 });
-// 
